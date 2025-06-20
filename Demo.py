43cb44556308576342ff282f8477c0b5db503e61
Sawsen-kl <sawsen.klai@gmail.com>
2025-06-19 21:43:49 -0400

    correction de Demo.py

--- Demo.py
+++ Demo.py
@@ -15,8 +15,8 @@ class MyApp(QWidget):
 
         self.inputField = QLineEdit()
         button = QPushButton('&Say Hello', clicked=self.sayHello)
-        button.clicked.connect(self.sayHello)
-        self.output = QTextEdit()
+        #button.clicked.connect(self.sayHello)
+        #self.output = QTextEdit()
         self.output = QTextEdit()
 
         layout.addWidget(self.inputField)
@@ -32,7 +32,7 @@ class MyApp(QWidget):
 app = QApplication(sys.argv)
 app.setStyleSheet('''
     QWidget {
-                  fon-size: 25px
+                  font-size: 25px
             }
                   
     QPushButton {
